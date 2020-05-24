1ab7fbb27cf0a607703f8b102ec05acc53afdef5
GitOPEN <gitopen@gmail.com>
2020-05-24 15:58:28 +0800

    update setup.py version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/opengit/gitopenlib.git"
 EMAIL = "gitopen@gmail.com"
 AUTHOR = "gitopen"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
