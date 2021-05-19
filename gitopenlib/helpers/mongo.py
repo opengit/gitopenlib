9a2838514ec384b6447e11c438d9e94fa7578d50
gitopen@gmail.com <>
2021-05-19 10:19:49 +0800

    更新分页方法

--- gitopenlib/helpers/mongo.py
+++ gitopenlib/helpers/mongo.py
@@ -122,11 +122,11 @@ def aggregate_by_page_asyncio(
 
     chunks = list()
 
-    #  async def parse_(chunk):
-    #      parse_func(chunk)
+    async def parse_(chunk):
+        await parse_func(chunk)
 
     async def parse_main():
-        await asyncio.gather(*[parse_func(chunk) for chunk in chunks])
+        await asyncio.gather(*[parse_(chunk) for chunk in chunks])
 
     if open_log:
         log_file = open(log_file, "a+")
