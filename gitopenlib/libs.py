aafaea69ecece7c0ed25f9c331a5ddbea9245577
SunJiaJia <sunjiajia@MAC.local>
2021-07-26 11:00:30 +0800

    添加了libs.py用于导出常用的py

---
+++ gitopenlib/libs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   libs.py
+@Time    :   2021/07/26 10:54:16
+@Author  :   GitOPEN
+@Version :   1.0
+@Contact :   gitopen@gmail.com
+@License :   (C)Copyright 2021-2022
+@Desc    :   用于快捷导入常用的包
+"""
+
+__version__ = "0.1.0"
+
+
+from gitopenlib.helpers import mongo as gm
+from gitopenlib.indicators import statistics as gs
+from gitopenlib.utils import basics as gb
+from gitopenlib.utils import files as gf
+from gitopenlib.utils import plot as gp
+from gitopenlib.utils import wonders as gw
