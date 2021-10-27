9b4e868208f22e51d35afe0819baa7181a9e1fa6
GitOPEN <gitopen@gmail.com>
2021-10-27 13:20:31 +0800

    添加了并行处理函数

--- gitopenlib/utils/wonders.py
+++ gitopenlib/utils/wonders.py
@@ -5,22 +5,46 @@
 # @Author :  GitOPEN
 # @Email  :  gitopen@gmail.com
 # @Date   :  2021-02-01 10:25:51
-# @Description :  一些”魔术“方法
+# @Description :  一些高级功能用法
 
-__version__ = "0.2.1"
+__version__ = "0.3.1"
 
+import asyncio
 from functools import wraps
 from time import time
 from typing import Callable
 
 from gitopenlib.utils import basics as gb
+from gitopenlib.utils import wonders as gw
+
+
+@gw.timing
+def run_tasks_parallel(
+    data: list,
+    parse_func: Callable[[list], None],
+    slave_num: int = 4,
+):
+    """
+    并行处理数据的函数，加快处理速度。
+
+    Args:
+        data: list类型，需要被处理的数据。
+        parse_func: 处理数据的函数，自行定义。
+        slave_num: 并行任务数目。
+    """
+
+    async def parse_(loop, chunk):
+        await loop.run_in_executor(None, lambda: parse_func(chunk))
+
+    loop = asyncio.get_event_loop()
+    chunks = gb.chunks(data, slave_num)
+    loop.run_until_complete(asyncio.gather(*[parse_(loop, chunk) for chunk in chunks]))
 
 
 def timing(f: Callable):
     """A simple timer decorator.
 
     装饰器，用于统计函数的运行时间。
-
     """
 
     @wraps(f)
