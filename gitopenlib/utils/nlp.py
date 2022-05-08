5af35524d77357a12a1f11bc1fe5deed7bbc799a
GitOPEN <gitopen@gmail.com>
2022-05-08 17:35:21 +0800

    nlp.py中增加了中文、英文的切分词函数

--- gitopenlib/utils/nlp.py
+++ gitopenlib/utils/nlp.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 # Copyright (c) 2022
@@ -7,13 +6,18 @@
 # @Date   :  2022-01-15 22:47:56
 # @Description :  Powered by 存放NLP常用的一些工具函数
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
 
 import random
 import re
 import string
 
 import emoji
+import jieba
+import jieba.posseg as psg
+
+from gitopenlib.libs import *
+from gitopenlib.utils import nlp as gn
 
 
 def remove_punc(text, repl=""):
@@ -63,3 +67,62 @@ def text_has_emoji(text):
         if character in emoji.UNICODE_EMOJI:
             return True
     return False
+
+
+def chinese_word_cut(
+    text: str,
+    custom_dict_file=None,
+    stop_words: list = [],
+    reserved_flags: list = [],
+    remove_punc: bool = True,
+) -> list:
+    """
+    对一段文本进行切分词。
+
+    Args:
+        mytext : 文本内容
+        custom_dict_file : 自定义词典路径
+        stop_words : 停用词列表
+        reserved_flags : 切分词后保留哪些词性的词汇
+
+    Returns:
+        分词列表
+    """
+    if custom_dict_file:
+        jieba.load_userdict(custom_dict_file)
+
+    jieba.initialize()
+
+    # jieba分词
+    word_list = []
+    # 切分结果
+    seg_list = psg.cut(text)
+    for seg_word in seg_list:
+        if remove_punc:
+            word = gn.remove_punc(seg_word.word)
+        else:
+            word = seg_word.word
+        find = 0
+        for stop_word in stop_words:
+            if stop_word == word or len(word) < 2:
+                find = 1
+                break
+        if len(reserved_flags) > 1:
+            if find == 0 and seg_word.flag in reserved_flags:
+                word_list.append(word)
+        else:
+            if find == 0:
+                word_list.append(word)
+
+    return word_list
+
+
+def english_word_cut(
+    text: str,
+    stop_words: list = [],
+    remove_punc: bool = True,
+) -> list:
+    """对英文句子分词（按照空格）"""
+    if remove_punc:
+        text = gn.remove_punc(text)
+    return [word for word in text.split() if word not in set(stop_words)]
