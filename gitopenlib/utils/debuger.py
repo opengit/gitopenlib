99146ee1c44eb7bddf8c84b8dea4e60f9941398c
GitOPEN <gitopen@gmail.com>
2022-12-09 21:33:38 +0800

    add  function to debugger.py

--- gitopenlib/utils/debuger.py
+++ gitopenlib/utils/debuger.py
@@ -7,9 +7,28 @@
 # @Date   :  2022-09-17 11:31:23
 # @Description :  Some useful things about debug.
 
-__version__ = "0.1.01"
+__version__ = "0.2.01"
 
 import sys
+import traceback
+
+
+def catch_except_run(func, args):
+    """运行指定函数，并捕获异常
+
+    Args:
+        func (function): 指定函数。
+        args (tuple): 函数参数。
+
+    Returns:
+        function: 若未报错，返回函数执行结果；若报错，返回None。
+
+    """
+    try:
+        return func(*args)
+    except Exception:
+        traceback.print_exc()
+        return None
 
 
 def exit(status: int = 0):
